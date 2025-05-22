e051c275e39bbdb9f380715d0b91406ffe584ee2
Lena Gampenrieder <if24b255@technikum-wien.at>
2025-05-22 16:12:43 +0200

    finaller clean up

--- src/find_spaceship.js
+++ src/find_spaceship.js
@@ -5,10 +5,8 @@ function findSpaceShip() {}
 findSpaceShip.prototype.add = function(string_map) {
 
 	const MAX_ROWS = 5;
-	const MAX_COLUMNS = 9;
 
 	if(!string_map.includes("X")){ return "Spaceship lost forever."}
-
 
     const rows = string_map.split("\n"); //rows ist ein Array | an jedem Index ist ein Teil von string_map der hinter einem \n gelegen hat
 
@@ -17,19 +15,16 @@ findSpaceShip.prototype.add = function(string_map) {
     
     for (let i = 0; i < rows.length; i++) {
         const row = rows[i];
-        const xIndex = row.indexOf("X");
+        const xIndex = row.indexOf("X"); //indexOf retourniert -1 falls der Parameter nicht im String gefunden wird
 
         if (xIndex !== -1) {
 			y = i;
 			if(y > MAX_ROWS){ return "Spaceship lost forever."; }
 
 			if(x === 0) {x = xIndex;}
-			else { return "to many Spaceships - unclear which one is Alfs";}
-            
+			else { return "to many Spaceships - unclear which one is Alfs";} 
         }
     }
-
-	
 
     return [x, y];
 };

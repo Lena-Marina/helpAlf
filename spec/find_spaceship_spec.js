47468ce86ff87926bce5619f64a1beb04cd1ab24
Lena Gampenrieder <if24b255@technikum-wien.at>
2025-05-22 15:37:06 +0200

    2nd Test Case - TDD Triangle Step 1, 2 and 3

--- spec/find_spaceship_spec.js
+++ spec/find_spaceship_spec.js
@@ -6,4 +6,8 @@ describe('find Spaceship', function() {
 	it('should return [7,2] for "..........\n..........\n.......X..\n..........\n..........\n.........."', function() {
 		expect(shipFinder.add('..........\n..........\n.......X..\n..........\n..........\n..........')).toEqual([7,2]);
 	});
+
+	it('should return [3,4] for "..........\n..........\n..........\n..........\n...X......\n.........."', function() {
+		expect(shipFinder.add('..........\n..........\n..........\n..........\n...X......\n..........')).toEqual([3,4]);
+	});
 });

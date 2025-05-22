9ad323f851ec2acfa4ce12f4474a92a973e3ad24
Lena Gampenrieder <if24b255@technikum-wien.at>
2025-05-22 15:49:26 +0200

    6th Test Case - TDD Triangle Step 1

--- spec/find_spaceship_spec.js
+++ spec/find_spaceship_spec.js
@@ -3,6 +3,7 @@ describe('find Spaceship', function() {
 		shipFinder = new findSpaceShip();
 	});
 
+	//normal cases
 	it('should return [7,2] for "..........\n..........\n.......X..\n..........\n..........\n.........."', function() {
 		expect(shipFinder.add('..........\n..........\n.......X..\n..........\n..........\n..........')).toEqual([7,2]);
 	});
@@ -11,6 +12,7 @@ describe('find Spaceship', function() {
 		expect(shipFinder.add('..........\n..........\n..........\n..........\n...X......\n..........')).toEqual([3,4]);
 	});
 
+	//no ship
 	it('should return "Spaceship lost forever." for "..........\n..........\n..........\n..........\n..........\n.........."', function() {
 		expect(shipFinder.add('..........\n..........\n..........\n..........\n..........\n..........')).toEqual("Spaceship lost forever.");
 	});
@@ -22,5 +24,12 @@ describe('find Spaceship', function() {
 	it('should return "Spaceship lost forever." for "..........\n..........\n.....7....\n..........\n..........\n.........."', function() {
 		expect(shipFinder.add('..........\n..........\n.....7....\n..........\n..........\n..........')).toEqual("Spaceship lost forever.");
 	});
+
+	//to many ships
+	it('should return "to many Spaceships. - unclear which one is Alfs" for "..........\n..........\n.......X..\n..........\n.....X....\n.........."', function() {
+		expect(shipFinder.add('..........\n..........\n.......X..\n..........\n.....X....\n..........')).toEqual("Spaceship lost forever.");
+	});
+
+
 });
 

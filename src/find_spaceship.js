567f5416eb5b1c60b27f380517415d8d3f290c37
Lena Gampenrieder <if24b255@technikum-wien.at>
2025-05-22 15:43:51 +0200

    3nd Test Case - TDD Triangle Step 3

--- src/find_spaceship.js
+++ src/find_spaceship.js
@@ -6,11 +6,12 @@ findSpaceShip.prototype.add = function(string_map) {
 
 	if(!string_map.includes("X")){ return "Spaceship lost forever."}
 
-    const rows = string_map.split("\n"); 
 
-    let y = 0;
+    const rows = string_map.split("\n"); //rows ist ein Array | an jedem Index ist ein Teil von string_map der hinter einem \n gelegen hat
+
     let x = 0;
-
+	let y = 0;
+    
     for (let i = 0; i < rows.length; i++) {
         const row = rows[i];
         const xIndex = row.indexOf("X");
